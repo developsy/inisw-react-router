95435e6e6fd1c027e550d5d8fa60a924c6bf9faf
An Sol <apdlvmf486@gmail.com>
2023-04-21 10:55:33 +0900

    feat: 컴포넌트로 속성 전달 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -7,14 +7,23 @@ import Members from "./components/Members";
 import SongList from "./components/SongList";
 
 const App = () => {
+  const [members] = useState([
+    { name: "Maggie Adams", photo: "photos/photos/Mag.png" },
+    { name: "Sammie Purcell", photo: "photos/photos/Sam.png" },
+    { name: "Tim Purcell", photo: "photos/photos/Tim.png" },
+    { name: "Scott King", photo: "photos/photos/King.png" },
+    { name: "Johnny Pike", photo: "photos/photos/JPike.jpg" },
+    { name: "Toby Ruckert", photo: "photos/photos/Toby.jpg" },
+  ]);
+
   return (
     <Router>
       <div className="container">
         <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/members" element={<Members />}></Route>
+          <Route path="/about" element={<About title={"여우와 늙다리들"} />}></Route>
+          <Route path="/members" element={<Members members={members} />}></Route>
           <Route path="/songs" element={<SongList />}></Route>
         </Routes>
       </div>
--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
-const Members = () => {
+const Members = (props) => {
+  let imgstyle = { width: 90, height: 80 };
+  let list = props.members.map((member) => {
+    return (
+      <div className="col-sm-6 col-md-4 col-lg-3" key={member.name}>
+        <img src={member.photo} className="img-thumbnail" alt={member.name} style={imgstyle} />
+        <br />
+        <h6>{member.name}</h6>
+        <br />
+        <br />
+      </div>
+    );
+  });
   return (
-    <div className="card card-body">
-      <h2>Memebers</h2>
+    <div>
+      <h2 class="m-4">Members</h2>
+      <div className="container">
+        <div className="row">{list}</div>
+      </div>
     </div>
   );
 };
