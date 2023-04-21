30a909064d8d089fe13529fef028d8c188c121fa
An Sol <apdlvmf486@gmail.com>
2023-04-21 15:29:49 +0900

    feat: lazy loading 구현 -webpackchunknam 플러그인 -pMinDelay

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import React, { useState } from "react";
 // Hash router: 사용하면 url에 /#이 추가됨.
 // #이란? 한 html 파일 안에서 여러 개의 위치를 잡는 기능을 함. 문서가 길면 a태그로 이름을 지정하여 이름만 입력하면 그 부분으로 이동할 수 있다.
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./components/Home";
-import About from "./components/About";
-import Members from "./components/Members";
-import SongList from "./components/SongList";
-//import SongDetail from "./components/SongDetail";
-import Player from "./components/Player";
-import NotFound from "./components/NotFound";
+import pMinDelay from "p-min-delay";
+
+//설치한 webpackchunkname 플러그인을 이용하여 여러 개의 js파일을 하나의 청크로 묶는다.
+//pMinDelay로 로딩 시간을 1초로 강제.
+const Header = React.lazy(() => pMinDelay(import(/* webpackChunkName:"home" */ "./components/Header"), 1000));
+const Home = React.lazy(() => pMinDelay(import(/* webpackChunkName:"home" */ "./components/Home"), 1000));
+const NotFound = React.lazy(() => pMinDelay(import(/* webpackChunkName:"home" */ "./components/NotFound"), 1000));
+
+const Loading = React.lazy(() => pMinDelay(import(/* webpackChunkName:"home" */ "./components/Loading"), 1000));
+
+const About = React.lazy(() => pMinDelay(import(/* webpackChunkName:"about" */ "./components/About"), 1000));
+const Members = React.lazy(() => pMinDelay(import(/* webpackChunkName:"members" */ "./components/Members"), 1000));
+
+const SongList = React.lazy(() => pMinDelay(import(/* webpackChunkName:"songs" */ "./components/SongList"), 1000));
+const Player = React.lazy(() => pMinDelay(import(/* webpackChunkName:"songs" */ "./components/Player"), 1000));
 
 const App = () => {
   const [songs] = useState([
@@ -34,30 +41,33 @@ const App = () => {
   ]);
 
   //이곳에서 컴포넌트에 전달할 state를 결정한다.
+  //React.Suspense를 사용하면 로딩 중에 보여줄 화면을 보여줄 수 있다.
   return (
-    <Router>
-      <div className="container">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About title={"여우와 늙다리들"} />}></Route>
-          <Route path="/members" element={<Members members={members} />}></Route>
-          <Route path="/songs" element={<SongList songs={songs} />}>
-            <Route
-              index
-              element={
-                <>
-                  <hr />
-                  <h3>재생 중인 곡 없음</h3>
-                </>
-              }
-            ></Route>
-            <Route path=":id" element={<Player songs={songs} />} />
-          </Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-      </div>
-    </Router>
+    <React.Suspense fallback={<Loading />}>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About title={"여우와 늙다리들"} />}></Route>
+            <Route path="/members" element={<Members members={members} />}></Route>
+            <Route path="/songs" element={<SongList songs={songs} />}>
+              <Route
+                index
+                element={
+                  <>
+                    <hr />
+                    <h3>재생 중인 곡 없음</h3>
+                  </>
+                }
+              ></Route>
+              <Route path=":id" element={<Player songs={songs} />} />
+            </Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </React.Suspense>
   );
 };
 
